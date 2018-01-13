03aab61180a950e9199dd8a62a8c261d434e1e70
Serena <sjsantoni@gmail.com>
2018-01-12 16:29:46 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
 	name = "datawrangler",
 	version = "0.0.1",
-	install_requires=["pandas"]
+	install_requires=["pandas"],
 	packages=find_packages(exclude=['contrib', 'docs', 'tests]),
 )
